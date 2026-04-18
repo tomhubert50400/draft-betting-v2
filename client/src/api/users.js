fe7010fdadfd1ac6b05354d0f60aed9f79257c63
tomhubert50400 <73112644+tomhubert50400@users.noreply.github.com>
2026-04-18 13:01:23 +0900

    update users.js

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -1,5 +1,11 @@
 import { apiFetch } from './client';
-export const fetchLeaderboard = () => apiFetch('/api/users/leaderboard');
+export const fetchLeaderboard = (filters = {}) => {
+  const params = new URLSearchParams();
+  if (filters.match_id) params.set('match_id', filters.match_id);
+  if (filters.series_id) params.set('series_id', filters.series_id);
+  const qs = params.toString();
+  return apiFetch(`/api/users/leaderboard${qs ? `?${qs}` : ''}`);
+};
 export const fetchUser = (id) => apiFetch(`/api/users/${id}`);
 export const fetchUserBets = (id) => apiFetch(`/api/users/${id}/bets`);
 export const fetchChampionStats = (id) => apiFetch(`/api/users/${id}/champion-stats`);
