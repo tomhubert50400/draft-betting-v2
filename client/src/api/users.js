26ee33f4303d6acc6fc581453908fe675a06994b
tomhubert50400 <73112644+tomhubert50400@users.noreply.github.com>
2026-04-18 11:11:26 +0900

    update users.js

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -2,3 +2,4 @@ import { apiFetch } from './client';
 export const fetchLeaderboard = () => apiFetch('/api/users/leaderboard');
 export const fetchUser = (id) => apiFetch(`/api/users/${id}`);
 export const fetchUserBets = (id) => apiFetch(`/api/users/${id}/bets`);
+export const fetchChampionStats = (id) => apiFetch(`/api/users/${id}/champion-stats`);
